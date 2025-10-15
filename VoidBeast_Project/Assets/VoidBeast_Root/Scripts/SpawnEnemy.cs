f88faae1ac1fba4bd058514bfbca15c8a32dec08
Zhey10 <Zhey10@users.noreply.github.com>
2025-10-16 01:51:21 +0200

    Day Night boton interactuar y spawn enemies

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/SpawnEnemy.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/SpawnEnemy.cs
@@ -1,24 +1,56 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    private bool nightStarted;
-    [SerializeField] object enemy;
-    [SerializeField] Transform spawner;
-    private int enemyQuantity;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform spawnerTransform;
+    [SerializeField] int enemyQuantity;
+    [SerializeField] TMP_Text enemyText;
+    [SerializeField] float spawnRadius = 300f;
+    private int maxGroupSize;
+    private bool enemiesSpawned = false;
+    private List<GameObject> activeEnemies = new List<GameObject>();
+
+
+
     void Update()
     {
-        if (nightStarted)
+        if (DayNightSystem.Instance == null)
+            return;
+
+        if (DayNightSystem.Instance.isNight && !enemiesSpawned)
         {
-           // for()
-           // Instantiate(enemy, spawner,Quaternion );
-            nightStarted = false;
+            enemyQuantity = DayNightSystem.Instance.enemyQuantity;
+            SpawnEnemies();
+            enemiesSpawned = true;
         }
+        if (DayNightSystem.Instance.isDay && enemiesSpawned)
+        {
+            enemiesSpawned = false;
+        }
+        if (DayNightSystem.Instance.isNight)
+        {
+            int enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            enemyText.text = "Enemigos: " + enemyCount;
+            if (enemyCount == 0) { DayNightSystem.Instance.ToDay(); }
+        }
+
+    }
+    private void SpawnEnemies()
+    {
+        for (int i = 0; i < enemyQuantity; i++)
+        {
+            float angle = i * Mathf.PI * 2f / enemyQuantity;
+            float x = Mathf.Cos(angle) * spawnRadius;
+            float z = Mathf.Sin(angle) * spawnRadius;
+            Vector3 spawnPos = spawnerTransform.position + new Vector3(x, 0, z);
+            Instantiate(enemyPrefab, spawnPos, Quaternion.LookRotation(spawnerTransform.position - spawnPos));
+
+        }
+
+
     }
 }
