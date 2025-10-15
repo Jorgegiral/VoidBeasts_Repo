4b2aad6f0c312015e5772a92f6169e01abe815c6
Zhey10 <private.adriansoler@gmail.com>
2025-10-15 10:30:42 +0200

    inicio daynight

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/DayNightSystem.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/DayNightSystem.cs
@@ -1,7 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DayNightSystem : MonoBehaviour
 {
+    public int dayNumber;
+    public int nightNumber;
+    public bool isDay;
+    public bool isNight;
+    [SerializeField] TMP_Text dayNigtText;
+    [SerializeField] Volume GlobalVolume;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
