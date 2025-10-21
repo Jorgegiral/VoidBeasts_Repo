843cf260e8fd44990d179fa0f328a6ad5bd3a5c8
Zhey10 <private.adriansoler@gmail.com>
2025-10-21 13:18:41 +0200

    image script

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Menu/Menu.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Menu/Menu.cs
@@ -1,9 +1,16 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] Image backgroundImage;
+    [SerializeField] Sprite backgroundDay;
+    [SerializeField] Sprite backgroundNight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
         
     }
