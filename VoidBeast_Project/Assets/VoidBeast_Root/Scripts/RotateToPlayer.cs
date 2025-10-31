75e471d5751eed06c7d1fbec9eca34ff5691884d
Zhey10 <private.adriansoler@gmail.com>
2025-10-31 09:19:21 +0100

    rotatePlayer

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/RotateToPlayer.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/RotateToPlayer.cs
@@ -28,8 +28,26 @@ public class RotateToPlayer : MonoBehaviour
 
         transform.rotation = rotation;
     }
+    public void RotateOnStopMoving()
+    {
+        Ray centerRay = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-    
+        Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 mousePoint = mouseRay.GetPoint(maximumLength);
+        Vector3 cameraPoint = centerRay.origin;
+
+        shootDirection = (mousePoint - cameraPoint).normalized;
+        shootDirection.y = 0f;
+        shootDirection.Normalize();
+        rotation = Quaternion.LookRotation(shootDirection);
+        float rotationSpeed = 5f;
+        Debug.Log("test");
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+
     public Quaternion GetRotation()
     {
         return rotation;
