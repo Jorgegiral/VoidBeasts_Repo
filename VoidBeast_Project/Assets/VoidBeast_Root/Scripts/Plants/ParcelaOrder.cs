f82d521c8e5a11cdfcea8e22ccb0529d1123d6c1
Zhey10 <Zhey10@users.noreply.github.com>
2025-11-12 01:23:47 +0100

    vfx

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Plants/ParcelaOrder.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Plants/ParcelaOrder.cs
@@ -4,5 +4,25 @@ public class ParcelaOrder : MonoBehaviour
 {
     public Parcela[] parcelas;
     [SerializeField] GameObject recolectVFX;
+    private void Start()
+    {
+        DayNightSystem.Instance.RegisterParcelaOrder(this);
 
+    }
+    public void PlayRecolect()
+    {
+        bool parcelasLlenas = false;
+        foreach(Parcela p in parcelas)
+        {
+            if (p.PlantIsFull())
+            {
+                parcelasLlenas = true;
+                break;
+            }
+        }
+        if (parcelasLlenas)
+        {
+            recolectVFX.SetActive(true);
+        }
+    }
 }
