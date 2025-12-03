2d4e5443c6ce73cf87388bda41df732e80b01666
Zhey10 <private.adriansoler@gmail.com>
2025-12-03 13:05:20 +0100

    tower and turret, tower script

---
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Build/Tower.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Collider range;
+    [SerializeField] GameObject cannonPoint;
+    [SerializeField] GameObject bulletVFX;
+    [Header("testers")]
+    [SerializeField] GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
