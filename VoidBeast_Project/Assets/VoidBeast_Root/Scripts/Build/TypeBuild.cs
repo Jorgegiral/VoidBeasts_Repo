634c4850d13dc1fae3c60497f478e2352bee19b4
Zhey10 <Zhey10@users.noreply.github.com>
2025-12-09 19:25:40 +0100

    mejoras y muro

--- VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Build/TypeBuild.cs
+++ VoidBeast_Project/Assets/VoidBeast_Root/Scripts/Build/TypeBuild.cs
@@ -9,28 +9,9 @@ public class TypeBuild : ScriptableObject
     public Vector3 clickOffSet;
     public Vector3 placeOffSet;
     public BuildType type;
-    public int level;
 
-    private void Awake()
-    {
-        CheckLevel();
-        CheckModel();
-    }
-    private void CheckLevel()
-    {
-        if (type.ToString() == "Build") return;
 
-        if (type.ToString() == "Tower") level = UpgradeManager.instance.towerLevel;
-        if (type.ToString() == "Wall") level = UpgradeManager.instance.wallLevel;
-    }
-    private void CheckModel()
-    {
-        if (type.ToString() == "Build") return;
-        if (level == 1) build = levelModels[0];
-        if (level == 2) build = levelModels[1];
-        if (level == 3) build = levelModels[2];
 
-    }
     public enum BuildType
     {
         Tower,
